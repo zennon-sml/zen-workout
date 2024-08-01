473678355b6647e00163642cb91c8ad4d008e0d4
zennon-sml <zennonsampaioml@gmail.com>
2024-07-31 21:29:54 -0300

    eslint rocketseat

---
+++ app/components/navBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <div>
+        <nav className="fixed top-0 w-full flex justify-between gap-2 p-2 bg-slate-800">home</nav>
+        </div>
+    )
+}
